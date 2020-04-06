efa48be5956e1ff5265cb9bd0f94e392f05e1e9c
loreabad6 <lore.abad6@gmail.com>
2020-04-06 12:17:25 +0200

    call sf for set_crs test

--- tests/testthat/test_crs.R
+++ tests/testthat/test_crs.R
@@ -1,8 +1,8 @@
-# test_that("st_set_crs sets the crs for edges and nodes", {
-#   net = as_sfnetwork(roxel) %>%
-#     st_set_crs(NA)
-#   expect_equal(st_crs(activate(net,'nodes')), st_crs(activate(net,'edges')))
-# })
+test_that("st_set_crs sets the crs for edges and nodes", {
+  net = as_sfnetwork(roxel) %>%
+    sf::st_set_crs(NA)
+  expect_equal(st_crs(activate(net,'nodes')), st_crs(activate(net,'edges')))
+})
 
 test_that("st_transform changes crs for edges and nodes", {
   net = as_sfnetwork(roxel) %>%
