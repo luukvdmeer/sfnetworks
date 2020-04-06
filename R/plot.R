dbeef5c9ebec902117df02bdb658ba0230e97169
loreabad6 <lore.abad6@gmail.com>
2020-04-06 18:03:15 +0200

    Update plot method Refs #17

--- R/plot.R
+++ R/plot.R
@@ -4,17 +4,23 @@
 #'
 #' @param x object of class sfnetwork
 #' @param ... further parameters passed to \code{\link[sf]{plot}}
+#' @importFrom graphics plot
 #' @export
 plot.sfnetwork = function(x, ...){
   dots = list(...)
-  # print(dots)
+
   pch_missing = is.null(dots$pch)
 
-  sfn = sf::st_geometry(sf::st_as_sf(activate(x, 'nodes')))
-  sfe = sf::st_geometry(sf::st_as_sf(activate(x, 'edges')))
-  sfgraph = c(sfn, sfe)
+  sfn = sf::st_geometry(sf::st_as_sf(activate(x, "nodes")))
 
-  dots$x = sfgraph
+  if (has_spatially_explicit_edges(x)) {
+    sfe = sf::st_geometry(sf::st_as_sf(activate(x, "edges")))
+    sfgraph = c(sfn, sfe)
+    dots$x = sfgraph
+  } else {
+    dots$x = sfn
+  }
+
   dots$pch = if (pch_missing) 20 else dots$pch
   do.call(plot, dots)
 }
