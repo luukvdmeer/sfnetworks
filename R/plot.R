81a829417abfed25b516449a6a1a9765cade2fd4
luukvdmeer <luukvandermeer@live.nl>
2020-04-09 13:22:30 +0200

    Tidy

--- R/plot.R
+++ R/plot.R
@@ -1,26 +1,25 @@
 #' Plot sfnetwork object
 #'
-#' Plot spatially explicit graph components (nodes and edges) of an sfnetwork on a map
+#' Plot spatially explicit graph components (nodes and edges) of an sfnetwork
+#' on a map.
 #'
-#' @param x object of class sfnetwork
-#' @param ... further parameters passed to \code{\link[sf]{plot}}
+#' @param x Object of class \code{\link{sfnetwork}}.
+#' @param ... Arguments passed on to \code{\link[sf]{plot}}.
 #' @importFrom graphics plot
 #' @export
-plot.sfnetwork = function(x, ...){
+plot.sfnetwork = function(x, ...) {
   dots = list(...)
-
-  pch_missing = is.null(dots$pch)
-
-  sfn = sf::st_geometry(sf::st_as_sf(activate(x, "nodes")))
-
+  nsf = sf::st_geometry(sf::st_as_sf(activate(x, "nodes"))) # Nodes
+  # Bind nodes and edges into one sf object if edges are spatially explicit.
   if (has_spatially_explicit_edges(x)) {
-    sfe = sf::st_geometry(sf::st_as_sf(activate(x, "edges")))
-    sfgraph = c(sfn, sfe)
-    dots$x = sfgraph
+    esf = sf::st_geometry(sf::st_as_sf(activate(x, "edges"))) # Edges
+    gsf = c(nsf, esf) # Full graph
+    dots$x = gsf
   } else {
-    dots$x = sfn
+    dots$x = nsf
   }
-
+  # Use pch of 20 by default.
+  pch_missing = is.null(dots$pch)
   dots$pch = if (pch_missing) 20 else dots$pch
   do.call(plot, dots)
 }
