7da89249bf5810186df97e832b666629f36f7f7d
luukvdmeer <luukvandermeer@live.nl>
2020-06-07 19:54:03 +0200

    Option to plot without making edges explicit

--- R/plot.R
+++ R/plot.R
@@ -4,22 +4,39 @@
 #'
 #' @param x Object of class \code{\link{sfnetwork}}.
 #'
+#' @param draw_lines If the edges of the network are spatially implicit, should
+#' straight lines be drawn between connected nodes? Defaults to \code{TRUE}.
+#' Ignored when the edges of the network are spatially explicit.
+#'
 #' @param ... Arguments passed on to \code{\link[sf]{plot}}
+#'
+#' @details This is a basic plotting functionality. For more advanced plotting,
+#' it is recommended to extract the nodes and edges from the network, and plot
+#' them separately with one of the many available spatial plotting functions
+#' as can be found in \code{sf}, \code{tmap}, \code{ggplot2}, \code{ggspatial},
+#' and others.
 #'
 #' @importFrom graphics plot
 #' @importFrom sf st_geometry
 #' @export
-plot.sfnetwork = function(x, ...) {
+plot.sfnetwork = function(x, draw_lines = TRUE, ...) {
   dots = list(...)
-  nsf = sf::st_geometry(activate(x, "nodes")) # Nodes.
+  # Get geometries of nodes.
+  nsf = sf::st_geometry(activate(x, "nodes"))
+  print(nsf)
+  # Combine node geometries with edge geometries if needed.
+  use_edges = TRUE
   if (! has_spatially_explicit_edges(x)) {
-    x = to_spatially_explicit_edges(x) # Draw edges if not spatially explicit.
+    if (draw_lines) {
+      x = to_spatially_explicit_edges(x)
+    } else {
+      use_edges = FALSE
+    }
   }
-  esf = sf::st_geometry(activate(x, "edges")) # Edges.
-  gsf = c(nsf, esf) # Full graph.
-  dots$x = gsf
+  dots$x = if (use_edges) c(nsf, sf::st_geometry(activate(x, "edges"))) else nsf
   # Use pch of 20 by default.
   pch_missing = is.null(dots$pch)
   dots$pch = if (pch_missing) 20 else dots$pch
+  # Plot.
   do.call(plot, dots)
 }
