8ed9a7b86650535849c8aa6d269fc9dab9ce0fed
luukvdmeer <luukvandermeer@live.nl>
2020-06-09 18:39:41 +0200

    Fewer reexports

--- R/tidygraph.R
+++ R/tidygraph.R
@@ -6,26 +6,6 @@ tidygraph::activate
 #' @export
 tidygraph::active
 
-#' @importFrom tidygraph .E
-#' @export
-tidygraph::`.E`
-
-#' @importFrom tidygraph .G
-#' @export
-tidygraph::`.G`
-
-#' @importFrom tidygraph .N
-#' @export
-tidygraph::`.N`
-
-#' @importFrom tidygraph %>%
-#' @export
-tidygraph::`%>%`
-
-#' @importFrom tidygraph as_tibble
-#' @export
-tidygraph::as_tibble
-
 #' @importFrom tidygraph as_tbl_graph
 #' @export
 as_tbl_graph.sfnetwork = function(x, ...) {
