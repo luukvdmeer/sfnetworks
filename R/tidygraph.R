3394f5b1d6f302b34d281eac020f7d7c0a3bc33a
luukvdmeer <luukvandermeer@live.nl>
2020-06-10 19:06:44 +0200

    Debug and sanitize morphing

--- R/tidygraph.R
+++ R/tidygraph.R
@@ -13,57 +13,31 @@ as_tbl_graph.sfnetwork = function(x, ...) {
   x
 }
 
-#' @importFrom tidygraph morph
-#' @export
-morph.sfnetwork = function(.data, .f, ...) {
-  morphed = tryCatch(
-    NextMethod(),
-    error = function(e) tidygraph::morph(as_tbl_graph(.data), .f, ...)
-  )
-  if (! all(sapply(morphed, is.sfnetwork))) {
-    tryCatch(
-      morphed_tbg_to_morphed_sfn(morphed),
-      error = function(e) morphed
-    )
-  } else {
-    structure(
-      morphed,
-      class = c("morphed_sfnetwork", class(morphed)),
-      .orig_graph = .data,
-      .morpher = attr(morphed, ".morpher")
-    )
-  }
-}
-
-#' @importFrom tidygraph unmorph
-#' @export
-unmorph.morphed_sfnetwork = function(.data) {
-  morphed_tbg = morphed_sfn_to_morphed_tbg(.data)
-  unmorphed_tbg = tidygraph::unmorph(morphed_tbg)
-  tbg_to_sfn(unmorphed_tbg)
-}
-
 tbg_to_sfn = function(.data) {
   class(.data) = c("sfnetwork", class(.data))
   .data
 }
 
-morphed_sfn_to_morphed_tbg = function(.data) {
-  morphed_tbg = lapply(.data, as_tbl_graph)
-  structure(
-    morphed_tbg,
-    class = setdiff(class(.data), "morphed_sfnetwork"),
-    .orig_graph = as_tbl_graph(attr(.data, ".orig_graph")),
-    .morpher = attr(.data, ".morpher")
+#' @importFrom tidygraph morph
+#' @export
+morph.sfnetwork = function(.data, .f, ...) {
+  # Morph using tidygraphs morphing functionality.
+  # First convert to tbl_graph if sfnetwork object gives errors.
+  morphed = tryCatch(
+    NextMethod(),
+    error = function(e) tidygraph::morph(as_tbl_graph(.data), .f, ...)
   )
-}
-
-morphed_tbg_to_morphed_sfn = function(.data) {
-  morphed_sfn = lapply(.data, function(x) as_sfnetwork(x, force = TRUE))
+  # Try to convert morphed elements into sfnetwork objects.
+  # If not possible, simply return as morphed_tbl_graph.
+  tryCatch(
+    {morphed_sfn = lapply(morphed, function(x) as_sfnetwork(x, force = TRUE))},
+    error = function(e) return(morphed)
+  )
+  # Return as object of class morphed_sfnetwork.
   structure(
-    morphed_sfn,
-    class = c("morphed_sfnetwork", class(.data)),
-    .orig_graph = as_sfnetwork(attr(.data, ".orig_graph"), force = TRUE),
-    .morpher = attr(.data, ".morpher")
+    morphed,
+    class = c("morphed_sfnetwork", class(morphed)),
+    .orig_graph = as_sfnetwork(attr(morphed, ".orig_graph"), force = TRUE),
+    .morpher = attr(morphed, ".morpher")
   )
 }
