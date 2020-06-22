ef3b0c30766f7fa03417f9e9665e80e940d26f41
Robin Lovelace <rob00x@gmail.com>
2020-06-22 22:29:52 +0100

    Add benchmark for #6

---
+++ bench/create.R
@@ -0,0 +1,33 @@
+pkgload::load_all()
+
+bench::press(n = seq(from = 1, to = nrow(roxel), length.out = 5),
+             {
+               bench::mark(
+                 time_unit = "ms",
+                 as_sfnetwork(roxel[1:n, ])
+               )
+             }
+             )
+
+# # comparison with stplanr (commented out to reduce dependencies)
+# library(sfnetworks)
+# system.time({
+#   net = as_sfnetwork(roxel)
+# })
+# system.time({
+#   net2 = stplanr::SpatialLinesNetwork(roxel)
+# })
+# pryr::object_size(net)
+# pryr::object_size(net2)
+#
+# res = bench::press(n = seq(from = 10, to = nrow(roxel), length.out = 5),
+#              {
+#                bench::mark(
+#                  check = FALSE,
+#                  time_unit = "ms",
+#                  stplanr::SpatialLinesNetwork(roxel[1:n, ]),
+#                  sfnetworks::as_sfnetwork(roxel[1:n, ])
+#                )
+#              }
+# )
+# ggplot2::autoplot(res)
