21056a29d7f30f0e0708b9e654f6a4e23d173437
luukvdmeer <luukvandermeer@live.nl>
2020-10-17 20:02:30 +0200

    Disable outdated geometry test

--- tests/testthat/test_geometry.R
+++ tests/testthat/test_geometry.R
@@ -1,8 +1,9 @@
-test_that('st_set_geometry gives an error when replacing nodes geometry', {
-  net = roxel %>% as_sfnetwork()
-  new_geom = net %>% st_geometry()
-  expect_error(net %>% sf::st_set_geometry(new_geom))
-})
+# This is not true anymore as long as the replacement geometry is equal to the original
+# test_that('st_set_geometry gives an error when replacing nodes geometry', {
+#   net = roxel %>% as_sfnetwork()
+#   new_geom = net %>% st_geometry()
+#   expect_error(net %>% sf::st_set_geometry(new_geom))
+# })
 
 test_that('st_set_geometry(NULL) for activated nodes changes the class to tbl_graph', {
   net = roxel %>% as_sfnetwork()
@@ -20,7 +21,6 @@ test_that('st_set_geometry gives an error when replacing edges geometry CRS', {
   new_geom = st_geometry(st_transform(roxel, 3035))
   expect_error(net %>% activate('edges') %>% sf::st_set_geometry(new_geom))
 })
-
 
 # Cannot test because GEOS does not load correctly with my sf library
 # test_that('st_set_geometry gives an error when replacing edges geometry endpoints', {
