71e4e3d0cd68e49a99be98bde67a6e112c2fbb8c
loreabad6 <lore.abad6@gmail.com>
2020-11-01 22:50:22 +0100

    Register S3 autoplot method for sfnetwork object

--- R/plot.R
+++ R/plot.R
@@ -54,11 +54,44 @@ plot.sfnetwork = function(x, draw_lines = TRUE, ...) {
   do.call(plot, dots)
 }
 
+#' Autoplot sfnetwork object
+#'
+#' Autoplot an object of class \code{\link{sfnetwork}} as a ggplot object.
+#'
+#' @param object An object of class \code{\link{sfnetwork}}.
+#' @param ... ignored
+#'
+#' @details This is a basic ggploting functionality, for a quick
+#' assessment of the network but on a \code{ggplot2} format.
+#'
+#' @examples
+#' library(ggplot2)
+#' library(sf)
+#' net = as_sfnetwork(roxel)
+#'
+#' # Quick overview of the network in `ggplot2` style
+#' autoplot(net)
+#'
+#' # Other `ggplot2` elements can be added
+#' points = net %>%
+#'   st_bbox() %>%
+#'   st_as_sfc() %>%
+#'   st_sample(10, type = 'random') %>%
+#'   st_set_crs(4326) %>%
+#'   st_cast('POINT')
+#'
+#' autoplot(net) +
+#'    # The theme can be customized
+#'    theme_minimal() +
+#'    # Labels can be added
+#'    labs(title = 'Nice ggplot') +
+#'    # And extra `geom_sf` layers can be included
+#'    geom_sf(data = points, color = 'red', size = 2)
 #' @importFrom sf st_as_sf
-autoplot.sfnetwork = function(x) {
+autoplot.sfnetwork = function(object, ...) {
   ggplot2::ggplot() +
-    ggplot2::geom_sf(data = st_as_sf(x, 'nodes')) +
-    ggplot2::geom_sf(data = st_as_sf(x, 'edges'))
+    ggplot2::geom_sf(data = st_as_sf(object, 'nodes')) +
+    ggplot2::geom_sf(data = st_as_sf(object, 'edges'))
 }
 
 # from: https://github.com/r-spatial/sf/blob/master/R/tidyverse.R
@@ -67,37 +100,74 @@ autoplot.sfnetwork = function(x) {
 # nocov start
 
 .onLoad = function(...) {
-  # has_ggplot2_3.0 =
-  #   requireNamespace("ggplot2", quietly = TRUE) &&
-  #   utils::packageVersion("ggplot2") >= "3.0.0"
-  #
-  # if (has_ggplot2_3.0)
+  has_ggplot2_3.0 =
+    requireNamespace("ggplot2", quietly = TRUE) &&
+    utils::packageVersion("ggplot2") >= "3.0.0"
 
-  register_s3_method("ggplot2", "autoplot", "sfnetwork")
+  if (has_ggplot2_3.0)
+    s3_register("ggplot2::autoplot", "sfnetwork")
+
   invisible()
 }
 
-register_s3_method <- function(pkg, generic, class, fun = NULL) {
-  stopifnot(is.character(pkg), length(pkg) == 1)
+# Register S3 helper
+# Code copied from: https://github.com/r-lib/vctrs/blob/master/R/register-s3.R
+# As suggested here: https://vctrs.r-lib.org/reference/s3_register.html
+s3_register = function(generic, class, method = NULL) {
   stopifnot(is.character(generic), length(generic) == 1)
   stopifnot(is.character(class), length(class) == 1)
 
-  if (is.null(fun)) {
-    fun <- get(paste0(generic, ".", class), envir = parent.frame())
-  } else {
-    stopifnot(is.function(fun))
+  pieces = strsplit(generic, "::")[[1]]
+  stopifnot(length(pieces) == 2)
+  package = pieces[[1]]
+  generic = pieces[[2]]
+
+  caller = parent.frame()
+
+  get_method_env = function() {
+    top = topenv(caller)
+    if (isNamespace(top)) {
+      asNamespace(environmentName(top))
+    } else {
+      caller
+    }
+  }
+  get_method = function(method, env) {
+    if (is.null(method)) {
+      get(paste0(generic, ".", class), envir = get_method_env())
+    } else {
+      method
+    }
   }
 
-  if (pkg %in% loadedNamespaces()) {
-    registerS3method(generic, class, fun, envir = asNamespace(pkg))
-  }
+  method_fn = get_method(method)
+  stopifnot(is.function(method_fn))
 
   # Always register hook in case package is later unloaded & reloaded
   setHook(
-    packageEvent(pkg, "onLoad"),
+    packageEvent(package, "onLoad"),
     function(...) {
-      registerS3method(generic, class, fun, envir = asNamespace(pkg))
+      ns = asNamespace(package)
+
+      # Refresh the method, it might have been updated by `devtools::load_all()`
+      method_fn = get_method(method)
+
+      registerS3method(generic, class, method_fn, envir = ns)
     }
   )
+
+  # Avoid registration failures during loading (pkgload or regular)
+  if (!isNamespaceLoaded(package)) {
+    return(invisible())
+  }
+
+  envir = asNamespace(package)
+
+  # Only register if generic can be accessed
+  if (exists(generic, envir)) {
+    registerS3method(generic, class, method_fn, envir = envir)
+  }
+
+  invisible()
 }
 # nocov end
