e6a15e63d130835a61dc9b8368437e9349fc31c6
loreabad6 <lore.abad6@gmail.com>
2021-04-18 16:40:53 +0200

    Reverse CRS change

--- tests/testthat/test_blend.R
+++ tests/testthat/test_blend.R
@@ -3,7 +3,7 @@ library(igraph)
 library(dplyr)
 node1 = st_point(c(0, 0))
 node2 = st_point(c(1, 0))
-edge = st_sfc(st_linestring(c(node1, node2)), crs = 4326)
+edge = st_sfc(st_linestring(c(node1, node2)))
 
 net = as_sfnetwork(edge)
 
@@ -11,7 +11,7 @@ pois = data.frame(
     poi_type = c("bakery", "butcher", "market"),
     x = c(0, 0.6, 0.3), y = c(0.1, 0.1, 0)
   ) %>%
-  st_as_sf(coords = c("x", "y"), crs = 4326)
+  st_as_sf(coords = c("x", "y"))
 
 test_that("st_network_blend with tolerance argument too large gives a
           warning and does not perform split", {
