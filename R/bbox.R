cf6f9308e7727e7fd339d283fdf184db0a846694
Luuk van der Meer <luukvandermeer@live.nl>
2024-08-03 12:57:52 +0200

    refactor: Tidy st_network_bbox function :construction:

--- R/bbox.R
+++ R/bbox.R
@@ -56,13 +56,10 @@ st_network_bbox = function(x, ...) {
 #' @export
 st_network_bbox.sfnetwork = function(x, ...) {
   # Extract bbox from nodes and edges.
-  nodes_bbox = st_bbox(st_geometry(x, "nodes"), ...)
-  edges_bbox = st_bbox(st_geometry(x, "edges"), ...)
+  nodes_bbox = st_bbox(pull_node_geom(x), ...)
+  edges_bbox = st_bbox(pull_edge_geom(x), ...)
   # Take most extreme coordinates to form the network bbox.
-  x_bbox = nodes_bbox
-  x_bbox["xmin"] = min(nodes_bbox["xmin"], edges_bbox["xmin"])
-  x_bbox["ymin"] = min(nodes_bbox["ymin"], edges_bbox["ymin"])
-  x_bbox["xmax"] = max(nodes_bbox["xmax"], edges_bbox["xmax"])
-  x_bbox["ymax"] = max(nodes_bbox["ymax"], edges_bbox["ymax"])
-  x_bbox
+  merge_bboxes(nodes_bbox, edges_bbox)
 }
+
+
