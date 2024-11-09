4bc5b072af37c32db7098a311eb3d4bc9c1504ac
Luuk van der Meer <luukvandermeer@live.nl>
2024-11-09 23:22:10 +0100

    breaking: Restructure spatial grouping functions :warning:

--- R/group.R
+++ R/group.R
@@ -1,32 +1,23 @@
 #' Group nodes based on spatial distance
 #'
-#' This function forms a spatial extension to the
+#' These functions forms a spatial extension to the
 #' \code{\link[tidygraph:group_graph]{grouping}} functions in tidygraph,
 #' allowing to detect communities with spatial clustering algorithms.
-#'
-#' @param dist The distance within which nodes are clustered together. Should
-#' be specified in meters.
-#'
-#' @param algorithm The spatial clustering algorithm to use. See Details.
 #'
 #' @param use_network_distance Should the distance between nodes be computed as
 #' the distance over the network (using \code{\link{st_network_distance}}?
 #' Defaults to \code{TRUE}. If set to \code{FALSE}, the straight-line distance
 #' (using \code{\link[sf]{st_distance}}) is computed instead.
 #'
-#' @param min_nodes The minimum number of nodes in each cluster. Defaults to 1.
+#' @param ... Additional arguments passed on to the clustering algorithm.
 #'
-#' @param ... Additional arguments passed on to the clustering algorithm. See
-#' Details.
-#'
-#' @details The currently supported spatial clustering algorithms are the
-#' following:
-#'
-#' \itemize{
-#'   \item \code{dbscan}: Uses density-based spatial clustering as implemented
-#'   in the \code{\link[dbscan]{dbscan}} function of the dbscan package. This
-#'   requires the dbscan package to be installed.
-#' }
+#' @details Just as with all grouping functions in tidygraph, spatial grouping
+#' functions are meant to be called inside tidygraph verbs such as
+#' \code{\link[tidygraph]{mutate}} or \code{\link[tidygraph]{filter}}, where
+#' the network that is currently being worked on is known and thus not needed
+#' as an argument to the function. If you want to use an algorithm outside of
+#' the tidygraph framework you can use \code{\link[tidygraph]{with_graph}} to
+#' set the context temporarily while the algorithm is being evaluated.
 #'
 #' @returns A numeric vector with the membership for each node in the network.
 #' The enumeration happens in order based on group size progressing from the
@@ -37,30 +28,41 @@
 #'
 #' play_geometric(10, 0.5) |>
 #'   activate(nodes) |>
-#'   mutate(group = group_spatial(0.25))
+#'   mutate(group = group_spatial_dbscan(0.25))
 #'
-#' @importFrom stats as.dist
+#' @name group_spatial
+NULL
+
+#' @describeIn group_spatial Uses density-based spatial clustering as
+#' implemented in the \code{\link[dbscan]{dbscan}} function of the dbscan
+#' package. This requires the dbscan package to be installed. Each node marked
+#' as noise will form its own cluster.
+#'
+#' @param epsilon The value of the epsilon parameter for the DBSCAN clustering
+#' algorithm, defining the radius of the neighborhood of a node.
+#'
+#' @param min_pts The value of the minPts parameter for the DBSCAN clustering
+#' algorithm, defining the minimum number of points in the neighborhood to be
+#' considered a core point.
+#'
+#' @importFrom rlang check_installed
 #' @export
-group_spatial = function(dist, algorithm = "dbscan",
-                         use_network_distance = TRUE, min_nodes = 1, ...) {
+group_spatial_dbscan = function(epsilon, min_pts = 1,
+                                use_network_distance = TRUE, ...) {
+  check_installed("dbscan") # Package dbscan is required for this function.
   require_active_nodes()
-  if (use_network_distance) {
-    distmat = as.dist(st_network_distance(.G()))
-  } else {
-    distmat = as.dist(st_distance(.G()))
-  }
-  groups = switch(
-    algorithm,
-    dbscan = group_spatial_dbscan(distmat, dist, min_nodes, ...),
-    raise_unknown_input("algorithm", algorithm, c("dbscan"))
-  )
+  dists = make_distance_matrix(.G(), use_network_distance)
+  groups = dbscan::dbscan(dists, eps = epsilon, minPts = min_pts, ...)$cluster
   desc_enumeration(groups)
 }
 
-#' @importFrom rlang check_installed
-group_spatial_dbscan = function(x, dist, min_nodes, ...) {
-  check_installed("dbscan") # Package dbscan is required for this function.
-  dbscan::dbscan(x, eps = dist, minPts = min_nodes, ...)$cluster
+#' @importFrom stats as.dist
+make_distance_matrix = function(x, use_network_distance = TRUE) {
+  if (use_network_distance) {
+    as.dist(st_network_distance(x))
+  } else {
+    as.dist(st_distance(x))
+  }
 }
 
 # From https://github.com/thomasp85/tidygraph/blob/main/R/group.R
